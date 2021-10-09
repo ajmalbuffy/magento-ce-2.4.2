e9a88cc7802a7e309f722921c4a6d22357240b64
Ajmalriyaz <mhdajmalriyaz73@gmail.com>
2021-10-09 21:40:49 +0530

    Add:Adding the page title

--- app/code/LearnMagento/Tutorial/Controller/Index/Page.php
+++ app/code/LearnMagento/Tutorial/Controller/Index/Page.php
@@ -1,1 +1,26 @@
 <?php
+declare(strict_types=1);
+namespace LearnMagento\Tutorial\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\ResponseInterface;
+
+class Page implements ActionInterface{
+    private \Magento\Framework\View\Result\PageFactory $pageFactory;
+
+    /**
+     * @param \Magento\Framework\View\Result\PageFactory $pageFactory
+     */
+    public function __construct(\Magento\Framework\View\Result\PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+        $page = $this->pageFactory->create();
+        $page->getConfig()->getTitle()->set("First Page");
+        return $page;
+    }
+}
--- app/code/LearnMagento/Tutorial/registration.php
+++ app/code/LearnMagento/Tutorial/registration.php
@@ -1,1 +1,6 @@
 <?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'LearnMagento_Tutorial',
+    __DIR__
+);
